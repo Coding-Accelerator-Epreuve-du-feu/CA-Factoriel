4328bee92639f0f420bf7109fa093e2a8f9856fb
Matheos Romain <romain.matheos.31@gmail.com>
2021-10-11 10:58:21 +0200

    Ajout d'un commentaire d'explication et d'une condition pour la gestion d'erreur

--- Factoriel.js
+++ Factoriel.js
@@ -1,5 +1,10 @@
 #! node
+/* Script qui permet de calculer la factoriel d'un nombre */
 
+if (isNaN(process.argv[2]) || process.argv[2] < 2) {
+  console.log("Veuillez indiquer au script, un nombre supérieur à 2 en argument");
+  process.argv[2] = 1;
+}
 let fact = process.argv[2];
 
 function factor(fact) {
